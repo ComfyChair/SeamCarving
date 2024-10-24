084b709dbb913a446d2d864807434530d639c0cf
ComfyChair <unknown.entity@gmx.de>
2024-10-24 09:32:29 +0200

    Stage 2/6: Negative photo

--- Seam Carving/task/src/seamcarving/Main.kt
+++ Seam Carving/task/src/seamcarving/Main.kt
@@ -2,11 +2,41 @@ package seamcarving
 
 import java.awt.Color
 import java.awt.image.BufferedImage
-import java.io.ByteArrayOutputStream
 import java.io.File
 import javax.imageio.ImageIO
 
-fun main() {
+fun main(args: Array<String>) {
+    var inPath: String = ""
+    var outPath: String = ""
+    for (i in 0 until args.size step 2) {
+        val command = args[i]
+        when (command) {
+            "-in" -> inPath = args[i + 1]
+            "-out" -> outPath = args[i + 1]
+        }
+    }
+    if (inPath.isNotEmpty() && outPath.isNotEmpty()) {
+        val negative = createNegative(inPath)
+        ImageIO.write(negative, "png", File(outPath))
+    }
+}
+
+fun createNegative(inPath: String) : BufferedImage {
+    val image: BufferedImage = ImageIO.read(File(inPath))
+    for (x in 0 until image.width) {
+        for (y in 0 until image.height) {
+            val originalColor = Color(image.getRGB(x, y))
+            val negativeColor = Color(
+                255 - originalColor.red,
+                255 - originalColor.green,
+                255 - originalColor.blue).rgb
+            image.setRGB(x, y, negativeColor)
+        }
+    }
+    return image
+}
+
+private fun redCrossImage() {
     println("Enter rectangle width:")
     val width = readln().toInt()
     println("Enter rectangle height:")
@@ -14,11 +44,11 @@ fun main() {
     println("Enter output image name:")
     val imageName = readln()
 
-    val image = rectangleImage(width, height)
+    val image = createRedCrossImage(width, height)
     ImageIO.write(image, "PNG", File(imageName))
 }
 
-fun rectangleImage(width: Int, height: Int): BufferedImage {
+private fun createRedCrossImage(width: Int, height: Int): BufferedImage {
     val image = BufferedImage(width, height, BufferedImage.TYPE_INT_RGB)
     val graphics = image.createGraphics()
     graphics.color = Color.red
